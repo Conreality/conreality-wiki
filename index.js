3f5132d21132fa198d38927af7e38cc327df62f0
Arto Bendiken <arto@bendiken.net>
2015-12-14 03:56:56 +0100

    Ensured that headings have autogenerated IDs.

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ $(function() {
     var page = window.location.pathname.substring(1);
     window.location.href = 'https://github.com/conreality/wiki.conreality.org/edit/master/' + page + '.md';
   });
+  $('h1, h2, h3, h4, h5, h6').each(function() {
+    var self = $(this);
+    self.attr('id', self.text().replace(' ', '-'));
+  });
 });
